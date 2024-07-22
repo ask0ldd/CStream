12e11b0c6400236962fbdac3cc1019b062597b5d
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-07-22 18:59:21 +0200

    *edit* front : register component base features

--- csfront/src/app/pages/auth/register/register.component.ts
+++ csfront/src/app/pages/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  errorMessage = ""
+
+  public registerForm : FormGroup = this.fb.group({
+    username : [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(6)
+      ]
+    ],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–\\[{}\\]:;',?/*~$^+=<>]).{8,}$")
+      ]
+    ]
+  });
+
+  constructor(private fb: FormBuilder){ }
+
+  onSubmit() : void{
+    
+  }
+  
 }
