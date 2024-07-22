281c987a19695c571f35ee9afe0f3982d136a287
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-07-22 18:53:08 +0200

    *edit* front : creating shared module to share reactive form module with the auth components

--- csfront/src/app/app.module.ts
+++ csfront/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { LoginComponent } from './pages/auth/login/login.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { LoginComponent } from './pages/auth/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
