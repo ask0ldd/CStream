2cdff7b744f4f01a1cb3318057b4dcfbbc20de69
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-07-23 20:05:16 +0200

    *edit* front / registration form : new validation rules & new error messages when incorrect inputs

--- csfront/src/app/pages/auth/register/register.component.ts
+++ csfront/src/app/pages/auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IRegisterRequest } from 'src/app/interfaces/requests/IRegisterRequest';
 
 @Component({
   selector: 'app-register',
@@ -11,18 +12,19 @@ export class RegisterComponent {
   errorMessage = ""
 
   public registerForm : FormGroup = this.fb.group({
-    username : [
-      '',
-      [
-        Validators.required,
-        Validators.minLength(6)
-      ]
-    ],
     email: [
       '',
       [
         Validators.required,
         Validators.email
+      ]
+    ],
+    username : [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.pattern("^[a-zA-Z0-9_-]{6,16}$")
       ]
     ],
     password: [
@@ -32,13 +34,32 @@ export class RegisterComponent {
         Validators.minLength(8),
         Validators.pattern("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–\\[{}\\]:;',?/*~$^+=<>]).{8,}$")
       ]
-    ]
+    ],
+    passwordCheck: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–\\[{}\\]:;',?/*~$^+=<>]).{8,}$")
+      ]
+    ],
   });
 
   constructor(private fb: FormBuilder){ }
 
   onSubmit() : void{
-    
+    if(this.registerForm.get("password")?.value != this.registerForm.get("passwordCheck")?.value){
+      this.errorMessage = "The password & its confirmation don't match."
+      return;
+    }
+
+    if(this.registerForm.valid){
+      const registerRequest = this.registerForm.value as IRegisterRequest;
+    }else{
+      if(this.registerForm.get("password")?.status != "VALID") this.errorMessage = "Your password must contain at least one lowercase letter, one uppercase letter, one number, and one special character."
+      if(this.registerForm.get("email")?.status != "VALID") this.errorMessage = "The email address provided is not in a recognized format."
+      if(this.registerForm.get("username")?.status != "VALID") this.errorMessage = "Your username should contain 6 to 16 characters."
+    }
   }
   
 }
